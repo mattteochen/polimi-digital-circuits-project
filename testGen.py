88f6afd0c3361aa7d992b020d8eb0cd16eaf44f6
Francesco Buccoliero <fr.buccoliero@gmail.com>
2023-02-27 15:34:25 +0100

    Updated testgen

--- testGen.py
+++ testGen.py
@@ -1,169 +1,28 @@
 import random
 
 arrayLength = 65535
-numberOfRAMChunks = 100 # must be < arrayLength
+numberOfRAMChunks = 2000 # must be < arrayLength
 maxBlocks = 100
 maxBufferLength = 30
 randomResetProb = 0.2
+positions = ["00","01","10","11"]
 
 # variables to be replaced are wrapped in dollar sign
-template = '''
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
-USE ieee.std_logic_unsigned.ALL;
-USE std.textio.ALL;
 
-ENTITY project_tb IS
-END project_tb;
-
-ARCHITECTURE projecttb OF project_tb IS
-    CONSTANT CLOCK_PERIOD : TIME := 100 ns;
-    SIGNAL tb_done : STD_LOGIC;
-    SIGNAL mem_address : STD_LOGIC_VECTOR (15 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL tb_rst : STD_LOGIC := '0';
-    SIGNAL tb_start : STD_LOGIC := '0';
-    SIGNAL tb_clk : STD_LOGIC := '0';
-    SIGNAL mem_o_data, mem_i_data : STD_LOGIC_VECTOR (7 DOWNTO 0);
-    SIGNAL enable_wire : STD_LOGIC;
-    SIGNAL mem_we : STD_LOGIC;
-    SIGNAL tb_z0, tb_z1, tb_z2, tb_z3 : STD_LOGIC_VECTOR (7 DOWNTO 0);
-    SIGNAL tb_w : STD_LOGIC;
-
-    CONSTANT SCENARIOLENGTH : INTEGER := $scenarioLength$; 
-    SIGNAL scenario_rst : unsigned(0 TO SCENARIOLENGTH - 1)     := "00110" & "$scenarioReset$";
-    SIGNAL scenario_start : unsigned(0 TO SCENARIOLENGTH - 1)   := "00000" & "$scenarioStart$";
-    SIGNAL scenario_w : unsigned(0 TO SCENARIOLENGTH - 1)       := "00000" & "$scenarioW$";
-
-    TYPE ram_type IS ARRAY (65535 DOWNTO 0) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
-    SIGNAL RAM : ram_type := (  
-$ramFilling$
-                                OTHERS => STD_LOGIC_VECTOR(to_unsigned($fallbackValue$, 8)
-                            );
-                    
-    COMPONENT project_reti_logiche IS
-        PORT (
-            i_clk : IN STD_LOGIC;
-            i_rst : IN STD_LOGIC;
-            i_start : IN STD_LOGIC;
-            i_w : IN STD_LOGIC;
-
-            o_z0 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-            o_z1 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-            o_z2 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-            o_z3 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-            o_done : OUT STD_LOGIC;
-
-            o_mem_addr : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-            i_mem_data : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-            o_mem_we : OUT STD_LOGIC;
-            o_mem_en : OUT STD_LOGIC
-        );
-    END COMPONENT project_reti_logiche;
-
-BEGIN
-    UUT : project_reti_logiche
-    PORT MAP(
-        i_clk => tb_clk,
-        i_start => tb_start,
-        i_rst => tb_rst,
-        i_w => tb_w,
-
-        o_z0 => tb_z0,
-        o_z1 => tb_z1,
-        o_z2 => tb_z2,
-        o_z3 => tb_z3,
-        o_done => tb_done,
-
-        o_mem_addr => mem_address,
-        o_mem_en => enable_wire,
-        o_mem_we => mem_we,
-        i_mem_data => mem_o_data
-    );
-
-
-    -- Process for the clock generation
-    CLK_GEN : PROCESS IS
-    BEGIN
-        WAIT FOR CLOCK_PERIOD/2;
-        tb_clk <= NOT tb_clk;
-    END PROCESS CLK_GEN;
-
-
-    -- Process related to the memory
-    MEM : PROCESS (tb_clk)
-    BEGIN
-        IF tb_clk'event AND tb_clk = '1' THEN
-            IF enable_wire = '1' THEN
-                IF mem_we = '1' THEN
-                    RAM(conv_integer(mem_address)) <= mem_i_data;
-                    mem_o_data <= mem_i_data AFTER 1 ns;
-                ELSE
-                    mem_o_data <= RAM(conv_integer(mem_address)) AFTER 1 ns; 
-                END IF;
-                ASSERT (mem_we = '1' OR mem_we = '0') REPORT "o_mem_we in an unexpected state" SEVERITY failure;
-            END IF;
-            ASSERT (enable_wire = '1' OR enable_wire = '0') REPORT "o_mem_en in an unexpected state" SEVERITY failure;
-        END IF;
-    END PROCESS;
-    
-    -- This process provides the correct scenario on the signal controlled by the TB
-    createScenario : PROCESS (tb_clk)
-    BEGIN
-        IF tb_clk'event AND tb_clk = '0' THEN
-            tb_rst <= scenario_rst(0);
-            tb_w <= scenario_w(0);
-            tb_start <= scenario_start(0);
-            scenario_rst <= scenario_rst(1 TO SCENARIOLENGTH - 1) & '0';
-            scenario_w <= scenario_w(1 TO SCENARIOLENGTH - 1) & '0';
-            scenario_start <= scenario_start(1 TO SCENARIOLENGTH - 1) & '0';
-        END IF;
-    END PROCESS;
-
-    -- Process without sensitivity list designed to test the actual component.
-    testRoutine : PROCESS IS
-    BEGIN
-        mem_i_data <= "00000000";
-
-        WAIT UNTIL tb_rst = '1';
-        WAIT UNTIL tb_rst = '0';
-
-        ASSERT tb_done = '0' REPORT "TEST FALLITO (postreset DONE != 0 )" SEVERITY failure;
-        ASSERT tb_z0 = "00000000" REPORT "TEST FALLITO (postreset Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z0))) severity failure; 
-        ASSERT tb_z1 = "00000000" REPORT "TEST FALLITO (postreset Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z1))) severity failure; 
-        ASSERT tb_z2 = "00000000" REPORT "TEST FALLITO (postreset Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z2))) severity failure; 
-        ASSERT tb_z3 = "00000000" REPORT "TEST FALLITO (postreset Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z3))) severity failure; 
-        WAIT UNTIL tb_start = '1';
-        ASSERT tb_z0 = "00000000" REPORT "TEST FALLITO (poststart Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z0))) severity failure; 
-        ASSERT tb_z1 = "00000000" REPORT "TEST FALLITO (poststart Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z1))) severity failure; 
-        ASSERT tb_z2 = "00000000" REPORT "TEST FALLITO (poststart Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z2))) severity failure; 
-        ASSERT tb_z3 = "00000000" REPORT "TEST FALLITO (poststart Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z3))) severity failure; 
-        WAIT UNTIL tb_done = '1';
-
-        WAIT FOR CLOCK_PERIOD/2;
-        ASSERT tb_z2 = std_logic_vector(to_unsigned(162, 8))  REPORT "TEST FALLITO (Z2 ---) found " & integer'image(to_integer(unsigned(tb_z2))) severity failure; --. Expected  209  found " & integer'image(tb_z0))))  severity failure;
-        WAIT UNTIL tb_done = '0';
-        WAIT FOR CLOCK_PERIOD/2;
-        ASSERT tb_z0 = "00000000" REPORT "TEST FALLITO (postdone Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z0))) severity failure; 
-        ASSERT tb_z1 = "00000000" REPORT "TEST FALLITO (postdone Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z1))) severity failure; 
-        ASSERT tb_z2 = "00000000" REPORT "TEST FALLITO (postdone Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z2))) severity failure; 
-        ASSERT tb_z3 = "00000000" REPORT "TEST FALLITO (postdone Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z3))) severity failure; 
-        WAIT UNTIL tb_start = '1';
-        WAIT UNTIL tb_done = '1';
-        
-        WAIT FOR CLOCK_PERIOD/2;
-
-        ASSERT tb_z1 = std_logic_vector(to_unsigned(88, 8))  REPORT "TEST FALLITO (Z1 ---) found " & integer'image(to_integer(unsigned(tb_z1))) severity failure; --. Expected  209  found " & integer'image(tb_z0))))  severity failure;
-        ASSERT tb_z2 = std_logic_vector(to_unsigned(162, 8))  REPORT "TEST FALLITO (Z2 ---) found " & integer'image(to_integer(unsigned(tb_z2))) severity failure; --. Expected  209  found " & integer'image(tb_z0))))  severity failure;
-
-        ASSERT false REPORT "Simulation Ended! TEST PASSATO (EXAMPLE)" SEVERITY failure;
-    END PROCESS testRoutine;
-
-END projecttb;
+checkEmpty = '''
+\tASSERT tb_z0 = "00000000" REPORT "TEST FALLITO (postreset Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z0))) severity failure; 
+\tASSERT tb_z1 = "00000000" REPORT "TEST FALLITO (postreset Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z1))) severity failure; 
+\tASSERT tb_z2 = "00000000" REPORT "TEST FALLITO (postreset Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z2))) severity failure; 
+\tASSERT tb_z3 = "00000000" REPORT "TEST FALLITO (postreset Z0--Z3 != 0 ) found " & integer'image(to_integer(unsigned(tb_z3))) severity failure; 
 '''
 
+with open('template','r') as f:
+    template = f.read()
+    f.close()
+
+
 # returns a randomly long, sorted array of random valid RAM positions
-ramPositions = sorted(random.sample(range(arrayLength), k=random.randint(1,numberOfRAMChunks)))
+ramPositions = sorted(random.sample(range(arrayLength), k=random.randint(numberOfRAMChunks/2,numberOfRAMChunks)))
 # creates a dict with the aforementioned ram positions and a random 8bit value
 RAM = {p:random.randint(0,255) for p in ramPositions}
 ramFilling = ''
@@ -191,13 +50,20 @@ w = ''
 start = ''
 reset = ''
 
+testAsserts = ''
+
 for bn in range(numberOfBlocks):
-    pos = random.choice(["00","01","10","11"])
+    pos = random.choice(positions)
+    posDec = positions.index(pos)
+
     scenarioLength += 2
 
     # to get the memory address i generate a random int, cast to binary
     # and finally reverse it as the W has LSB first
-    memoryAddress = str(bin(random.randint(0,arrayLength))).split("0b")[1][::-1]
+    mem = random.randint(0,arrayLength)
+    memValue = RAM.get(mem, fallbackValue)
+    memoryAddress = str(bin(mem)).split("0b")[1][::-1]
+    
     scenarioLength += len(memoryAddress)
 
     bufferLength = random.randint(0,maxBufferLength)
@@ -211,14 +77,26 @@ for bn in range(numberOfBlocks):
     start += f"11{'1' * len(memoryAddress)}{buffer}0"
     reset += f"00{'0' * len(memoryAddress)}{buffer}{resetFlag}"
 
+    testAsserts += "\tWAIT UNTIL tb_start = '1';\n"
+    testAsserts += checkEmpty
+    testAsserts += "\tWAIT UNTIL tb_done = '1';\n"
+    testAsserts += "\tWAIT FOR CLOCK_PERIOD/2;\n"
+    testAsserts += f"\tASSERT tb_z{posDec} = std_logic_vector(to_unsigned({memValue}, 8)) severity failure;"
+    testAsserts += "\tWAIT UNTIL tb_done = '0';\n"
+    testAsserts += "\tWAIT FOR CLOCK_PERIOD/2;\n"
+    testAsserts += checkEmpty
+
 assert len(w) == len(start) == len(reset) == (scenarioLength - 5)
 
 template = template.replace("$scenarioW$",w)
 template = template.replace("$scenarioStart$",start)
 template = template.replace("$scenarioReset$",reset)
 template = template.replace("$scenarioLength$",str(scenarioLength))
-    
-print(template)
+template = template.replace("$testAsserts$",testAsserts)
+
+with open(f"tests/tb_{random.randint(0,100000000)}.vhd","w") as f:
+    f.write(template)
+    f.close()
 
 
 
